73b49c096418f8bfe2c101723d22b2e79101f42a
drmushi <drmushi@ncsu.edu>
2021-04-11 20:42:17 -0400

    Unit10

---
+++ apiscripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$("button").click(function(){
+var cat = $.get("https://aws.random.cat/meow");
+cat.done(function(response){
+$("randomcat").attr("src", response.message);
+
+})
+
+})
+
+})
+
+
+
