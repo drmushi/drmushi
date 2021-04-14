6348e5593d052b6eb63fccd4c7d516c79426cc15
drmushi <drmushi@ncsu.edu>
2021-04-14 09:58:38 -0400

    updatedAPI

--- apiscripts.js
+++ apiscripts.js
@@ -2,12 +2,10 @@ $(document).ready(function(){
 $("button").click(function(){
 var cat = $.get("https://aws.random.cat/meow");
 cat.done(function(response){
-$("randomcat").attr("src", response.message);
+$("randomcat").attr("src", response.file);
 
+});
 })
-
-})
-
 })
 
 
