85f5f2c5e61bd25896339d5277a52d7bc60008cd
drmushi <drmushi@ncsu.edu>
2021-05-06 10:18:23 -0400

    FinalProjectUpdates

--- postwebhooks.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("form").click(function(){
-        event.preventDefault ();
-        var webform = $.post("https://maker.ifttt.com/trigger/webform/with/key/1g3z2HSArrsyL9lDjhtVf", JSON.stringify({"value1":$("#name").val(), "value2": $("#email").val(), "value3": $("#message").val()}));
-    });
-})
